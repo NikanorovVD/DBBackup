d5505f481b33d4f692777630bc04e8dace8f3410
NikanorovVD <nikanorovvsevolod1@gmail.com>
2025-06-21 18:26:43 +0300

    Auto backup

---
+++ DBBackup/AutoBackupSheduler.cs
@@ -0,0 +1,47 @@
+﻿using Quartz;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DBBackup
+{
+    public static class AutoBackupSheduler<BackupServiceT> where BackupServiceT : IBackupService, new()
+    {
+        public static async Task StartAutoBackup(Database database)
+        {
+            IHost builder = Host.CreateDefaultBuilder()
+                 .ConfigureServices((cxt, services) =>
+                 {
+                     services.AddQuartz();
+                     services.AddQuartzHostedService(opt =>
+                     {
+                         opt.WaitForJobsToComplete = true;
+                     });
+                 }).Build();
+
+            ISchedulerFactory schedulerFactory = builder.Services.GetRequiredService<ISchedulerFactory>();
+            IScheduler scheduler = await schedulerFactory.GetScheduler();
+
+
+            IJobDetail job = JobBuilder.Create<BackupJob<BackupServiceT>>()
+                .WithIdentity("BackupJob")
+                .UsingJobData("User", database.Connection.User)
+                .UsingJobData("Password", database.Connection.Password)
+                .UsingJobData("Host", database.Connection.Host)
+                .UsingJobData("Port", database.Connection.Port)
+                .UsingJobData("Database", database.DatabaseName)
+                .Build();
+
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("BackupTrigger")
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(4)
+                    .RepeatForever())
+                .Build();
+
+            await scheduler.ScheduleJob(job, trigger);
+            await builder.RunAsync();
+        }
+    }
+}
---
+++ DBBackup/BackupJob.cs
@@ -0,0 +1,38 @@
+﻿using Quartz;
+
+namespace DBBackup
+{
+    public class BackupJob<BackupServiceT> : IJob where BackupServiceT : IBackupService, new()
+    {
+        private IBackupService _backupService;
+
+        public string User {  get; set; }
+        public string Password { get; set; }
+        public string Host {  get; set; }
+        public int Port { get; set; }
+        public string Database {  get; set; }
+
+        public BackupJob()
+        {
+            _backupService = new BackupServiceT();
+        }
+
+        public async Task Execute(IJobExecutionContext context)
+        {
+            Database database = new()
+            {
+                Connection = new()
+                {
+                    User = User,
+                    Password = Password,
+                    Host = Host,
+                    Port = Port
+                },
+                DatabaseName = Database
+            };
+
+            string path = $"dump_{DateTime.Now:yy-MM-dd-HH-mm-ss}.sql"; 
+            await _backupService.BackupDatabaseAsync(database, path);
+        }
+    }
+}
