c622585f51b9ec00fc2f2ece61d23c0521f8de46
NikanorovVD <nikanorovvsevolod1@gmail.com>
2025-06-23 14:56:50 +0300

    CLI

--- DBBackup/Program.cs
+++ DBBackup/Program.cs
@@ -3,15 +3,82 @@ using DBBackup.Configuration;
 using DBBackup.Postgres;
 using Microsoft.Extensions.Configuration;
 using Serilog;
+using System.CommandLine;
+
 
 namespace DBBackup
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+        static async Task<int> Main(string[] args)
+        {
+            Option<FileInfo> configOption = new("--config", "-c")
+            {
+                Description = "Файл с конфигурацией приложения в формате json",
+                DefaultValueFactory = parseResult => new FileInfo("appsettings.json")
+            };
+
+            Option<string> databaseOption = new("--database", "-db")
+            {
+                Description = "Имя базы данных",
+                Required = true,
+            };
+
+            Option<string> outPathOption = new("--out", "-o")
+            {
+                Description = "Путь для сохранения бекапа",
+                Required = true
+            };
+
+            Option<FileInfo> inPathOption = new("--backup", "-b")
+            {
+                Description = "Путь к бекапу для восстановления",
+                Required = true
+            };
+
+            Command autoCommand = new("auto", "Запускает процесс автоматического резервного копирования")
+            {
+                configOption,
+            };
+            autoCommand.SetAction(async parseResult => await AutoBackup(parseResult.GetValue(configOption)));
+
+            Command backupCommand = new("backup", "Создает резервную копию базы данных")
+            {
+                configOption,
+                databaseOption,
+                outPathOption
+            };
+            backupCommand.SetAction(async parseResult => await Backup(
+                parseResult.GetValue(configOption),
+                parseResult.GetValue(databaseOption),
+                parseResult.GetValue(outPathOption)
+                ));
+
+            Command restoreCommand = new("restore", "Восстанавливает базу данных из резервной копии")
+            {
+                configOption,
+                inPathOption,
+                databaseOption
+            };
+            restoreCommand.SetAction(async parseResult => await Restore(
+                parseResult.GetValue(configOption),
+                parseResult.GetValue(inPathOption),
+                parseResult.GetValue(databaseOption)
+                ));
+
+            RootCommand rootCommand = new("Приложение для автоматического резервного копирования баз данных");
+            rootCommand.Subcommands.Add(autoCommand);
+            rootCommand.Subcommands.Add(backupCommand);
+            rootCommand.Subcommands.Add(restoreCommand);
+
+            ParseResult parseResult = rootCommand.Parse(args);
+            return parseResult.Invoke();
+        }
+
+        private static async Task AutoBackup(FileInfo congigFile)
         {
             IConfigurationRoot config = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.Development.json")
+                .AddJsonFile(congigFile.FullName)
                 .Build();
 
             Settings? settings = config.Get<Settings>() ?? throw new Exception("Invalid settings");
@@ -22,7 +89,51 @@ namespace DBBackup
                 .ReadFrom.Configuration(config)
                 .CreateLogger();
 
-           await AutoBackupSheduler<PostgresBackupService>.StartAutoBackup(connection, settings.AutoBackups, settings.Email);
+            await AutoBackupSheduler<PostgresBackupService>.StartAutoBackup(connection, settings.AutoBackups, settings.Email);
+        }
+
+        private static async Task Backup(FileInfo congigFile, string databaseName, string path)
+        {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .AddJsonFile(congigFile.FullName)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .MinimumLevel.Information()
+                .CreateLogger();
+
+            Connection connection = config.GetSection("Connection").Get<Connection>() ?? throw new Exception("Invalid settings");
+            Database database = new Database()
+            {
+                Connection = connection,
+                DatabaseName = databaseName
+            };
+
+            IBackupService backupService = new PostgresBackupService();
+            await backupService.BackupDatabaseAsync(database, path);
+        }
+
+        private static async Task Restore(FileInfo congigFile, FileInfo backupFile, string databaseName)
+        {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .AddJsonFile(congigFile.FullName)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .MinimumLevel.Information()
+                .CreateLogger();
+
+            Connection connection = config.GetSection("Connection").Get<Connection>() ?? throw new Exception("Invalid settings");
+            Database database = new Database()
+            {
+                Connection = connection,
+                DatabaseName = databaseName
+            };
+
+            IBackupService backupService = new PostgresBackupService();
+            await backupService.RestoreDatabaseAsync(backupFile.FullName, database);
         }
     }
 }
